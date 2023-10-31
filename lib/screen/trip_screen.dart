98d9b75d33f1f9f7e6c0b08e70090997004f543c
alek0 <alek013@rambler.ru>
2023-10-31 08:49:26 +0300

    update

--- lib/screen/trip_screen.dart
+++ lib/screen/trip_screen.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import '../db/trip.dart';
 
 class TripScreen extends StatelessWidget {
-   TripScreen({Key? key}) : super(key: key);
-  List listTrip = [
-    Trip(id: 1, title: "Рязань", date:  DateTime(2023, 10, 20)),
-    Trip(id: 2, title: "Владимир", date:  DateTime(2023, 10, 24)),
-    Trip(id: 3, title: "Арзамас", date:  DateTime(2023, 10, 25)),
-    Trip(id: 4, title: "Рязань", date:  DateTime(2023, 10, 30))];
+   const TripScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    List listTrip = [
+      Trip(id: 1, title: "Рязань", date:  DateTime(2023, 10, 20)),
+      Trip(id: 2, title: "Владимир", date:  DateTime(2023, 10, 24)),
+      Trip(id: 3, title: "Арзамас", date:  DateTime(2023, 10, 25)),
+      Trip(id: 4, title: "Рязань", date:  DateTime(2023, 10, 30))];
     return Container(
       padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
       child: Column(
